3487061e242e9ecdc8c1a053603ab2d86c2d482e
juuzouvlone <vlonereyvetements@mail.ru>
2023-02-24 19:06:00 +0300

    1

---
+++ krestnol/Game/app/build/generated/source/buildConfig/debug/com/unaev/tictac/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.unaev.tictac;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.unaev.tictac";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "1.6";
+}
